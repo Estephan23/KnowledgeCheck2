84a56bdee96d8d21453ba37be7615a596c513e94
estep <estep@DESKTOP-5KEUCAJ>
2023-09-06 20:40:55 -0400

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    internal class Person
+    {
+        public string FirstName {g;   };
+        public string LastName;
+        public int Age;
+        public string Gender;
+    }
+}
